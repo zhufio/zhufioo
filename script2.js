137fa410c1b29306878cb0d8667789f7f37ab4be
zhufio <zhufi841@gmail.com>
2024-12-17 18:37:46 -0500

    ds

--- script2.js
+++ script2.js
@@ -1,23 +1,23 @@
 const wrestlerData1 = [
-        [1, "Hero Coles", "6 - 1 ", 12, 6, 0, "Hero Cho", "Cole Castor", "WLWWWWW"],
-        [2, "KT", "5 - 2 ", 12, 6, 0, "Trajanboy", "Kendrick Bush", "WWWWWLL"],
-        [3, "Task: Heat", "5 - 2 ", 11, 7, 0, "Heat", "Zack Task", "WWLWLWW"],
-        [4, "Indestructible Os", "3 - 4 ", 9, 0, 0, "Indestructible Isaac", "Owen Owl Oxley", "LLLWWWL"],
-        [5, "Frostbite Jays", "3 - 4 ", 8, -2, 0, "Frostbite Fred Bones", "Thaddeus Jay", "LLWLLWW"],
-        [6, "Rox Bops", "3 - 4 ", 6, -3, 0, "Benjamin Bops", "Will Rox", "WWLLWLL"],
-        [7, "Minty Blanks", "2 - 5 ", 4, -8, 0, "Minty Mania", "Blank Shadow", "LWLLLLW"],
-        [8, "Drago Kids", "1 - 6 ", 7, -6, 0, "The Kid", "Mason Drago", "LLWLLLL"]
+        [1, "Hero Coles", "6 - 1 ", 12, 6, 0, "Hero Cho", "Cole Castor"],
+        [2, "KT", "5 - 2 ", 12, 6, 0, "Trajanboy", "Kendrick Bush"],
+        [3, "Task: Heat", "5 - 2 ", 11, 7, 0, "Heat", "Zack Task"],
+        [4, "Indestructible Os", "3 - 4 ", 9, 0, 0, "Indestructible Isaac", "Owen Owl Oxley"],
+        [5, "Frostbite Jays", "3 - 4 ", 8, -2, 0, "Frostbite Fred Bones", "Thaddeus Jay"],
+        [6, "Rox Bops", "3 - 4 ", 6, -3, 0, "Benjamin Bops", "Will Rox"],
+        [7, "Minty Blanks", "2 - 5 ", 4, -8, 0, "Minty Mania", "Blank Shadow"],
+        [8, "Drago Kids", "1 - 6 ", 7, -6, 0, "The Kid", "Mason Drago"]
       ];
 
 const wrestlerData2 = [
-        [1, "Moon Jets", "6 - 1 ", 13, 8, 0, "Aiden Jet", "Kyle Moon", "LWWWWWW"],
-        [2, "Uber Cowboys", "4 - 3  ", 10, 2, 0, "Spike Cowboy", "Uber Squeezed Papaya", "LWLLWWW"],
-        [3, "Xander Landon", "4 - 3 ", 10, 1, 0, "Noah Landon", "Xander Velvet", "WLWLLWW"],
-        [4, "Otunga Reyes", "4 - 3 ", 8, -2, 0, "Jason Otunga", "Ralph Reyes", "WLWWWLL"],
-        [5, "Casual Rockets", "3 - 4 ", 9, 0, 0, "Casual Carlton", "Rocket Head", "LWLWWLL"],
-        [6, "Los Gloriosos", "3 - 4 ", 8, -1, 0, "Diego Torres", "Edwardo", "LLLWLWW"],
-        [7, "Cookie Deez", "3 - 4 ", 7, -1, 0, "Cooki's Monster", "Deez", "WWWLLLL"],
-        [8, "The BLT Gary", "1 - 6 ", 6, -7, 0, "The BLT Cupcake", "Gary Dallas", "WLLLLLL"]
+        [1, "Moon Jets", "6 - 1 ", 13, 8, 0, "Aiden Jet", "Kyle Moon"],
+        [2, "Uber Cowboys", "4 - 3  ", 10, 2, 0, "Spike Cowboy", "Uber Squeezed Papaya"],
+        [3, "Xander Landon", "4 - 3 ", 10, 1, 0, "Noah Landon", "Xander Velvet"],
+        [4, "Otunga Reyes", "4 - 3 ", 8, -2, 0, "Jason Otunga", "Ralph Reyes"],
+        [5, "Casual Rockets", "3 - 4 ", 9, 0, 0, "Casual Carlton", "Rocket Head"],
+        [6, "Los Gloriosos", "3 - 4 ", 8, -1, 0, "Diego Torres", "Edwardo"],
+        [7, "Cookie Deez", "3 - 4 ", 7, -1, 0, "Cooki's Monster", "Deez"],
+        [8, "The BLT Gary", "1 - 6 ", 6, -7, 0, "The BLT Cupcake", "Gary Dallas"]
       ];
 
 function generateTableData(data, tableId) {
